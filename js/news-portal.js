90e1e4275fa4308df9de90270a3dcf34a8f597b1
freelancersifat627e <freelancersifat627@gmail.comm>
2023-08-19 17:49:23 +0600

    compleat news portal project

--- js/news-portal.js
+++ js/news-portal.js
@@ -36,7 +36,93 @@ const showCategoryNews = (data , category_name) => {
     // console.log(data , category_name);
     document.getElementById('news-count').innerText = data.length;
     document.getElementById('category-name').innerText = category_name;
+    const newsContainer = document.getElementById('all_news');
+    newsContainer.innerHTML = '';
     data.forEach(singleNews =>{
-        console.log(singleNews);
+        // console.log(singleNews);
+        const {_id , image_url , title , details , author , total_view} = singleNews;
+        newsContainer.innerHTML += `
+        <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src= ${image_url} class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8 d-flex flex-column">
+            <div class="card-body">
+              <h5 class="card-title fw-bold">${title}</h5>
+              <p class="card-text">${details.slice(0 , 200)}...</p>
+            </div>
+            <div class="card-footer bg-white border-0 d-flex justify-content-between align-items-center">
+                <div class="d-flex">
+                    <img src= ${author.img? author.img : 'NO Img Found'} class="img-fluid rounded-circle me-2" alt="..." height="40px" width="40px">
+                    <div>
+                        <p class="p-0 m-0">${author.name? author.name : "Not available"}</p>
+                        <p class="p-0 m-0">${author.published_date}M</p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center">
+                    <i class="fas fa-eye me-2"></i>
+                    <P class="p-0 m-0">${total_view? total_view : "Not available"}M</P>
+                </div>
+                <div>
+                    <i class="fas fa-star"></i>
+                </div>
+                <div>
+                    <i class="fas fa-arrow-right" onclick ="fetchNewsDetail('${_id}')" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        `;
     });
+};
+
+const fetchNewsDetail = async news_id => {
+    const URL = `https://openapi.programming-hero.com/api/news/${news_id}`;
+    // console.log(URL);
+    try {
+        const res = await fetch(URL);
+        const data = await res.json();
+        showNewsDetail(data.data[0]);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const showNewsDetail = newsDetail => {
+    console.log(newsDetail);
+    const modalBody = document.getElementById('modal-body');
+    const {_id , others_info , image_url , title , details , author , total_view} = newsDetail;
+    modalBody.innerHTML = `
+    <div class="card mb-3">
+    <div class="row g-0">
+      <div class="col-md-12">
+        <img src= ${image_url} class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-12 d-flex flex-column">
+        <div class="card-body">
+          <h5 class="card-title fw-bold">${title} <span class="badge text-bg-warning">${others_info.is_trending? 'Trending' : 'Not Trending' }</span></h5>
+          <p class="card-text">${details}</p>
+        </div>
+        <div class="card-footer bg-white border-0 d-flex justify-content-between align-items-center">
+            <div class="d-flex">
+                <img src= ${author.img? author.img : 'NO Img Found'} class="img-fluid rounded-circle me-2" alt="..." height="40px" width="40px">
+                <div class="d-flex flex-column">
+                    <p class="p-0 m-0">${author.name? author.name : 'Not available'}</p>
+                    <p class="p-0 m-0">${author.published_date}M</p>
+                </div>
+            </div>
+            <div class="d-flex align-items-center">
+                <i class="fas fa-eye me-2"></i>
+                <P class="p-0 m-0">${total_view? total_view : 'not available'}M</P>
+            </div>
+            <div>
+                <i class="fas fa-star"></i>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
 };
